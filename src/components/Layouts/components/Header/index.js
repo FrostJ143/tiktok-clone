e32b81fe714f154b31c9699811b2c73627b71184
FrostJ143 <70150272+FrostJ143@users.noreply.github.com>
2023-04-07 12:42:19 +0700

    add css hearder and defaultlayout

---
+++ src/components/Layouts/components/Header/index.js
@@ -0,0 +1,17 @@
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+function Header() {
+    return (
+        <header className={cx("wrapper")}>
+            <div className={cx("innter")}>
+                {/* logo */
+                /* search*/}
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 // Layouts
-import { HeaderOnly } from "~/Layout";
+import { HeaderOnly } from "~/components/Layouts";
 
 import Home from "~/pages/Home";
 import Following from "~/pages/Following";
